56f431bfa01c668f50086c5852823c6efe033b18
raisavar <raisavar@users.noreply.github.com>
2025-06-21 07:04:19 +0300

    test(db): build configuration update

--- src/api.js
+++ src/api.js
@@ -185,3 +185,30 @@ class API {
 
 // Update 28
 module.exports = API;
+
+
+// API module for GuacamoleServer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 31
+module.exports = API;
