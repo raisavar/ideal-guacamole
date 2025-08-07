0e44b5cb99d8c274f0e1bfe7b2b40a164619cbe1
raisavar <raisavar@users.noreply.github.com>
2025-08-07 14:35:02 +0300

    style: performance optimization

--- src/api.js
+++ src/api.js
@@ -104,3 +104,30 @@ class API {
 
 // Update 19
 module.exports = API;
+
+
+// API module for GuacamoleServer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 21
+module.exports = API;
