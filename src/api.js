7d18e95a00cacb274ec83b403167b0eb26340c40
raisavar <raisavar@users.noreply.github.com>
2025-09-06 04:23:19 +0300

    refactor: fix compilation error

--- src/api.js
+++ src/api.js
@@ -23,3 +23,30 @@ class API {
 
 // Update 4
 module.exports = API;
+
+
+// API module for GuacamoleServer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 9
+module.exports = API;
