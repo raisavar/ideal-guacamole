a16c9cdc6bf09e059dc7cce71cabb15f1923e98c
raisavar <raisavar@users.noreply.github.com>
2025-10-22 13:42:34 +0300

    docs(tests): bug fix for edge case

--- src/api.js
+++ src/api.js
@@ -158,3 +158,30 @@ class API {
 
 // Update 26
 module.exports = API;
+
+
+// API module for GuacamoleServer
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 28
+module.exports = API;
